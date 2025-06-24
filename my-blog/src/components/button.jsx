87e661e44980d143297fcfa8aa7b37aa2fcb32b4
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-24 21:35:57 +0300

    made button component

---
+++ my-blog/src/components/button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+    return ( 
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
+            Get started
+        </button>
+     );
+}
+
+export default Button;
