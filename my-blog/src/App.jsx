28655cf73c1b1e281720964778ce4e3da6caddb0
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-24 21:41:39 +0300

    tested button component

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -1,10 +1,12 @@
-
+//do imports
+import Button from './components/button';
 import './App.css'
 
 function HomePage() {
   return ( 
     <>
       <h1 className='text-5xl font-bold'>Welcome to My Blog</h1>
+      <Button />
     </>
    );
 }
